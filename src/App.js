fd2880c15d26fd672d9352b1ee35ba6a881a8604
layouwen <layouwen@gmail.com>
2020-08-22 22:04:10 +0800

    简单实现页面导航

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import './App.css'
+import {Switch, Route} from 'react-router-dom'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Home from './pages/Home'
+import History from './pages/History'
+import About from './pages/About'
 
 function App() {
   return (
-    <div className="App">
-
+    <div className="app">
+      <Header/>
+        <Switch>
+          {/*<Route path="/" exact>*/}
+          {/*  <Home/>*/}
+          {/*</Route>*/}
+          <Route path="/" exact component={Home}/>
+          <Route path="/history" exact component={History}/>
+          <Route path="/about" exact component={About}/>
+        </Switch>
+      <Footer/>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
