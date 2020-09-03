f2c8a4afd137373ecae2204152dcc1f74d70f748
liangyouwen <layouwen@gmail.com>
2020-09-03 16:12:13 +0800

    美化注册页面的样式

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,20 +1,70 @@
-import React, {useRef} from 'react'
-import {observer} from 'mobx-react'
-import {useStores} from '../stores'
+import React from "react"
+import styled from "styled-components"
+import {Form, Input, Button, Checkbox} from "antd"
 
-const Component = observer(() => {
-  const {AuthStore} = useStores()
-  const inputRef = useRef()
+const Wraper = styled.div`
+  max-width: 600px;
+  padding: 20px;
+  margin: 30px auto;
+  box-shadow: 2px 2px 4px 0 rgba(0,0,0,.2);
+  border-radius: 4px;
+`
 
-  const bindChange = e => {
-    AuthStore.setUsername(inputRef.current.value)
+const Title = styled.h1`
+  margin-bottom: 10px;
+  text-align: center;
+`
+
+const layout = {
+  labelCol: {span: 4},
+  wrapperCol: {span: 20},
+}
+const tailLayout = {
+  wrapperCol: {offset: 4, span: 20},
+}
+
+const Component = () => {
+  const onFinish = values => {
+    console.log("Success:", values)
   }
+
+  const onFinishFailed = errorInfo => {
+    console.log("Failed:", errorInfo)
+  }
+
   return (
-    <>
-      <h1>Login: {AuthStore.values.username}</h1>
-      <input onChange={bindChange} ref={inputRef}/>
-    </>
+    <Wraper>
+      <Title>注册</Title>
+      <Form
+        {...layout}
+        name="basic"
+        initialValues={{remember: true}}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+      >
+        <Form.Item
+          label="Username"
+          name="username"
+          rules={[{required: true, message: "请输入账号!"}]}
+        >
+          <Input/>
+        </Form.Item>
+
+        <Form.Item
+          label="Password"
+          name="password"
+          rules={[{required: true, message: "请输入密码！"}]}
+        >
+          <Input.Password/>
+        </Form.Item>
+
+        <Form.Item {...tailLayout}>
+          <Button type="primary" htmlType="submit">注册</Button>
+        </Form.Item>
+      </Form>
+    </Wraper>
   )
-})
+}
+
 
 export default Component
